11b4dcbaa37e548a36d2b61787dc9a1277435da7
David Perez Millan <david@davidperezmillan.com>
2017-11-20 08:35:11 +0000

    Dinamicy path logger uri

--- airtrap/utilities/constantes.py
+++ airtrap/utilities/constantes.py
@@ -5,13 +5,15 @@ import socket
 
 global basepath
 basepathswitch = {
-    "davidperezmillan-metascrap-2716814":"/home/ubuntu/workspace/airtrap",
-    "davidperezmillan-mycrybot-5060192":"/home/ubuntu/workspace/airtrap",
     "alpha_server":"/home/david/script/conf/airtrap"
     }
+basepathlogswitch = {
+    "alpha_server":"/home/david/script/logs/airtrap"
+    }
+
 basepath=basepathswitch[socket.gethostname()]
-basepathlog = '{0}/logs/'.format(basepath)
-basepathlogplugins = '{0}/logs/plugins/'.format(basepath)
+basepathlog = basepathlogswitch[socket.gethostname()]
+basepathlogplugins = '{0}/plugins/'.format(basepathlog)
 
 
 
