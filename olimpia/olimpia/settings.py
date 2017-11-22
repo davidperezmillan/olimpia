1160858598c9a9f36a99320e78c32340d0681b64
David Perez Millan <david@davidperezmillan.com>
2017-11-22 14:46:46 +0000

    Solved settings.py logger

--- olimpia/olimpia/settings.py
+++ olimpia/olimpia/settings.py
@@ -17,7 +17,7 @@ import os, sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-print os.path.join(BASE_DIR, '../airtrap/utilities')
+# print os.path.join(BASE_DIR, '../airtrap/utilities')
 sys.path.insert(0, os.path.join(BASE_DIR, '../airtrap/utilities'))
 import constantes as cons
 
