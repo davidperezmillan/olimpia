d37db145e78c282af194617f8599a60cb41aeed7
David Perez Millan <david@davidperezmillan.com>
2017-11-24 10:05:50 +0000

    create ini file config Path

--- airtrap/utilities/constantes.py
+++ airtrap/utilities/constantes.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import socket
+# import socket
+# global basepath
+# basepathswitch = {
+#     "davidperezmillan-olimpia-5687265" : "/home/ubuntu/conf/airtrap/",
+#     "alpha_server":"/home/david/script/conf/airtrap/"
+#     }
+# basepathlogswitch = {
+#     "davidperezmillan-olimpia-5687265" : "/home/ubuntu/logs/airtrap/",
+#     "alpha_server":"/home/david/script/logs/airtrap/"
+#     }
 
-global basepath
-basepathswitch = {
-    "davidperezmillan-olimpia-5687265" : "/home/ubuntu/conf/airtrap/",
-    "alpha_server":"/home/david/script/conf/airtrap/"
-    }
-basepathlogswitch = {
-    "davidperezmillan-olimpia-5687265" : "/home/ubuntu/logs/airtrap/",
-    "alpha_server":"/home/david/script/logs/airtrap/"
-    }
+import os, sys
+import ConfigParser
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-basepath=basepathswitch[socket.gethostname()]
-basepathlog = basepathlogswitch[socket.gethostname()]
-basepathlogplugins = '{0}/plugins/'.format(basepathlog)
+try:
+    files = [os.path.join(BASE_DIR, 'airtrapConfig.ini')]
+    Config = ConfigParser.ConfigParser()
+    dataset = Config.read(files)
+    if len(dataset) != len(files):
+        raise ValueError("No existe el alguno de los ficheros {0} de configuracion, creenlos con el formato que tiene en el ejemplo ".format(files))
+    
+    basepath= Config.get('PATH','basepath')
+    basepathlog=Config.get('PATH','basepathlog')
+    basepathlogplugins = '{0}/plugins/'.format(basepathlog)
 
-
+except Exception as e:
+    sys.exit(e)
+   
 
 
