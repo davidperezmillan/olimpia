3f90b042342623d3d890031538f3bf5422bd225a
davidperezmillan <david@davidperezmillan.com>
2017-12-19 14:26:38 +0000

    Custom admin

--- olimpia/merc/admin.py
+++ olimpia/merc/admin.py
@@ -3,8 +3,25 @@ from django.contrib import admin
 # unregister your models here.
 from .models import Series, TelegramChatIds,TorrentServers, Plugins
 
-admin.site.register(Series)
+
 admin.site.register(TelegramChatIds)
 admin.site.register(TorrentServers)
 admin.site.register(Plugins)
 
+
+
+
+
+class SeriesAdmin(admin.ModelAdmin):
+    
+    def upper_case_name(self,obj):
+        return ("%s %s" % (obj.nombre, obj.quality)).upper()
+    
+    upper_case_name.short_description = 'Nombre Completo'
+    # ...
+    list_display = ('id','upper_case_name','nombre', 'quality', 'author')
+    list_filter = ['author','skipped','paussed','quality',]
+    search_fields = ['nombre']
+    
+    
+admin.site.register(Series, SeriesAdmin)
--- olimpia/olimpia/settings.py
+++ olimpia/olimpia/settings.py
@@ -38,7 +38,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'suit',
+    #'suit',
+    #'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
