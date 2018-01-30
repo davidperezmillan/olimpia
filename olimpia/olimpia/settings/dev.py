f8672732a9143452304646177627d0e70cd8ce27
davidperezmillan <david@davidperezmillan.com>
2018-01-30 13:22:52 +0000

    filter envs

--- olimpia/olimpia/settings/dev.py
+++ olimpia/olimpia/settings/dev.py
@@ -2,6 +2,19 @@ from .base import *
 
 LOGS_PATH=os.path.join(PROJECT_DIR, '../logs')
 
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3',
+        # 'NAME': '{0}/data/followingseries.sqlite3'.format(cons.basepath),
+        'NAME' : '/home/ubuntu/workspace/airconf/conf/data/followingseries.sqlite3',
+    }
+}
+
+
 # SECURITY WARNING: don't run with DEBUG turned on in production!
 DEBUG = True
 
