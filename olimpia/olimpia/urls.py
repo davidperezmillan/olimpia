202fcb6358bd7b7433be15b74048e569c085c58b
davidperezmillan <david@davidperezmillan.com>
2018-02-07 13:57:24 +0000

    Login project

--- olimpia/olimpia/urls.py
+++ olimpia/olimpia/urls.py
@@ -21,5 +21,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^hoor/', include('hoor.urls')),
 ]
