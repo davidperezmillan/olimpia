9515d277ee8162e7f14a788fc8c323bdb8650fc8
davidperezmillan <david@davidperezmillan.com>
2018-02-12 13:21:30 +0000

    modified:   manage.py 	deleted:    olimpia/settings/__init__.py 	deleted:    olimpia/settings/base.py 	deleted:    olimpia/settings/dev.py 	deleted:    olimpia/settings/prod.py 	modified:   olimpia/wsgi.py 	olimpia/settings.py

--- olimpia/manage.py
+++ olimpia/manage.py
@@ -5,5 +5,5 @@ from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
     
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "olimpia.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "olimpia.settings")
     execute_from_command_line(sys.argv)
