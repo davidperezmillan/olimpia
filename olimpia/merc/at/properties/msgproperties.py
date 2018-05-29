c27e890da3ba02124d8fcd86d507fa778aae83d6
davidperezmillan <david@davidperezmillan.com>
2018-05-29 11:27:27 +0000

    add properties AT

--- olimpia/merc/at/properties/msgproperties.py
+++ olimpia/merc/at/properties/msgproperties.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-MSG_TELEGRAM = {"header":"El usuario: {1} \n\r", 
+MSG_TELEGRAM = {"header":"{1} \n\r", 
     "footer":"\n\r'El Mercenario' le desea que disfrute de lo encontrado, Gracias por utilizar 'La Trampa del Aire - El Mercenario'",
     "item": "{0}{1}.  \n\r",
     "nothing":"{1} Circule, aqui no hay nada que ver....",
