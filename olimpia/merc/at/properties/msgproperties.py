2ca9e6b283bc16c9c94b0d504805d1ea647c87e5
davidperezmillan <david@davidperezmillan.com>
2018-05-29 08:59:10 +0000

    add properties AT

--- olimpia/merc/at/properties/msgproperties.py
+++ olimpia/merc/at/properties/msgproperties.py
@@ -4,7 +4,7 @@
 MSG_TELEGRAM = {"header":"El usuario: {1} \n\r", 
     "footer":"\n\r'El Mercenario' le desea que disfrute de lo encontrado, Gracias por utilizar 'La Trampa del Aire - El Mercenario'",
     "item": "{0}{1}.  \n\r",
-    "nothing":"Circulen, aqui no hay nada que ver....",
+    "nothing":"{1} Circule, aqui no hay nada que ver....",
     "organize":"Ya tenemos las librerias organizas con el nuevo contenido",
     "new":"Se ha anadido una nueva serie {0} [{1}]",
     }
