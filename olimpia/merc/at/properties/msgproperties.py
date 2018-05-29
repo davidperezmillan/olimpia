5dbf82b6fcc5f7864ae62f8a14eee80f3ba66205
davidperezmillan <david.perez.millan@gmail.com>
2018-05-29 10:23:56 +0200

    add msg telegram

--- olimpia/merc/at/properties/msgproperties.py
+++ olimpia/merc/at/properties/msgproperties.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-MSG_TELEGRAM = {"header":"'El Mercenario' ha puesto en cola {0} torrent para su descargas [{1}]:   \n\r", 
-    "footer":"\n\rEspero que lo disfruteis, Gracias por utilizar 'La Trampa del Aire - El Mercenario'",
-    "item": "{0} -- {1}.  \n\r",
-    "nothing":"Que pena 'El Mercenario' no ha encontrado nada de {} que enviar [{}].....",
+MSG_TELEGRAM = {"header":"\n\r", 
+    "footer":"\n\r'El Mercenario' le desea que disfrute de lo encontrado, Gracias por utilizar 'La Trampa del Aire - El Mercenario'",
+    "item": "{0}{1}.  \n\r",
+    "nothing":"Circulen, aqui no hay nada que ver....",
+    "organize":"Ya tenemos las librerias organizas con el nuevo contenido",
     }
