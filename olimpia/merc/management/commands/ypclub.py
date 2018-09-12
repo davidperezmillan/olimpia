22ef043b2c795c1e933419be581d876bae7ac7a3
davidperezmillan <david@davidperezmillan.com>
2018-09-12 07:20:59 +0000

    modificamos texto de msg

--- olimpia/merc/management/commands/ypclub.py
+++ olimpia/merc/management/commands/ypclub.py
@@ -173,25 +173,26 @@ class Command(BaseCommand):
 
         # Construimos y enviamos el mensaje
         if not options['test']:
-	    logger.info("Creamos el mensaje")
-	    try:
-            	msgHeader = "Hemos encontrado en yp {} de {} \n\r".format(len(listaTorrent), len(listaNoTorrent))
-	        sitems = ""
-        	sFinal = ""
-		for item in listaTorrent:
-			try:
-				elemento = item['title'].encode('utf-8').strip()
-				lTags = [x.encode('utf-8') for x in item["tags"]]
-        			sitems = "{0}{1}.\t {2}  \n\r".format(sitems,elemento.encode('utf-8'), lTags) 
-			except Exception:
-				logger.warn("titulo ilegible")
-				sitems = "{0}{1}.\t {2}  \n\r".format(sitems,"Titulo ilegible", lTags) 
-	        msg = "{0}{1}{2}".format(msgHeader,sitems, sFinal)
-		logger.info("Mensaje a enviar: {}".format(msg))
-		merc.at.hilos.utiles.sendTelegram(msg, user=self.author, receivers=self.receivers)
-	    except Exception, e:
-		logger.warn(e)
-		logger.error("caught exception, traceback =", exc_info=True)
+            try:
+                logger.info("Creamos el mensaje")
+                msgHeader = "Hemos encontrado en yp {} de {} \n\r".format(len(listaTorrent), len(listaNoTorrent))
+                sitems = ""
+                sFinal = ""
+                for item in listaTorrent:
+                    try:
+                        elemento = item['title'].encode('utf-8').strip()
+                        lTags = [x.encode('utf-8') for x in item["tags"]]
+                        sitems = "{0}{1}.\t {2}  \n\r".format(sitems,elemento.encode('utf-8'), lTags) 
+                    except Exception:
+                        logger.warn("titulo ilegible")
+                        sitems = "{0}{1}.\t {2}  \n\r".format(sitems,"Titulo ilegible", lTags)
+                sitems = "{0} \n\r".format(sitems)
+                msg = "{0}{1}{2}".format(msgHeader,sitems, sFinal)
+                logger.info("Mensaje a enviar: {}".format(msg))
+                merc.at.hilos.utiles.sendTelegram(msg, user=self.author, receivers=self.receivers)
+            except Exception, e:
+                logger.warn(e)
+                logger.error("caught exception, traceback =", exc_info=True)
     
 
    
